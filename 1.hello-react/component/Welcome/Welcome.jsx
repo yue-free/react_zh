9af3ea6e8cc85347deca9d9ff136ba7477c35a77
刘晨 <aaronlikearn@gmail.com>
2024-06-13 15:44:10 +0800

    first_react

---
+++ 1.hello-react/component/Welcome/Welcome.jsx
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+import './Welcome.css'
+
+export default class Hello extends Component{
+    render(){
+        return <h2 className="demo">welcome</h2>
+    }
+}
